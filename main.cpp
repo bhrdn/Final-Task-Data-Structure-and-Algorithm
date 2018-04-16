1d52a3cb0e3444ae794fae1686f3641845bf13a7
bhrdn <habibbahruddin1337@gmail.com>
2018-04-16 23:02:54 +0700

    reload main

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 
 int main(int argc, char const *argv[])
 {
-<<<<<<< HEAD
 	int choice;
 	do
 	{
@@ -16,53 +15,33 @@ int main(int argc, char const *argv[])
 		     << " 5. Exit" << endl
 		     << "\n[*] Enter your choice and press <enter>: ";
 		cin >> choice;
-=======
-
-	// nambahi conto tok
-
-	// do
-	// {
-	// 	showMenu();
-	// 	switch (choice)
-	// 	{
-	// 		case 1:
-	// 			showMenu('create');
-	// 			break;
->>>>>>> 4820485d6c8b44906211a1cd39b91926e4861aae
 
 		switch (choice)
 		{
-		case 1:
-			showMenu("create");
-			break;
+			case 1:
+				showMenu("create");
+				break;
 
-		case 2:
-			showMenu("read");
-			break;
+			case 2:
+				showMenu("read");
+				break;
 
-		case 3:
-			showMenu("update");
-			break;
+			case 3:
+				showMenu("update");
+				break;
 
-		case 4:
-			showMenu("delete");
-			break;
+			case 4:
+				showMenu("delete");
+				break;
 
-		case 5:
-			break;
+			case 5:
+				break;
 
-<<<<<<< HEAD
-		default:
-			cout << "[x] Empty menu.." << endl;
-			break;
+			default:
+				cout << "[x] Empty menu.." << endl;
+				break;
 		}
 	} while (choice != 5);
-=======
-	createList(LC);
-	AC = alokasi("Daerah A");
-	insertFirst(LC, AC);
 	
-	printInfo(LC);
->>>>>>> 4820485d6c8b44906211a1cd39b91926e4861aae
 	return 0;
 }
