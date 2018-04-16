726dc91257d9f020825f2203e2419f7099177243
bhrdn <habibbahruddin1337@gmail.com>
2018-04-16 22:56:30 +0700

    enable comment menu in main

--- main.cpp
+++ main.cpp
@@ -2,54 +2,45 @@
 
 int main(int argc, char const *argv[])
 {
-	// int choice;
-	// do
-	// {
-	// 	system("clear");
-	// 	cout << "=== DATA ALAT TRANSPORT ===" << endl;
-	// 	cout << endl
-	// 	     << " 1. Create" << endl
-	// 	     << " 2. Read" << endl
-	// 	     << " 3. Update" << endl
-	// 	     << " 4. Delete" << endl
-	// 	     << " 5. Exit" << endl
-	// 	     << "\n[*] Enter your choice and press <enter>: ";
-	// 	cin >> choice;
+	int choice;
+	do
+	{
+		system("clear");
+		cout << "=== DATA ALAT TRANSPORT ===" << endl;
+		cout << endl
+		     << " 1. Create" << endl
+		     << " 2. Read" << endl
+		     << " 3. Update" << endl
+		     << " 4. Delete" << endl
+		     << " 5. Exit" << endl
+		     << "\n[*] Enter your choice and press <enter>: ";
+		cin >> choice;
 
-	// 	switch (choice)
-	// 	{
-	// 	case 1:
-	// 		showMenu("create");
-	// 		break;
+		switch (choice)
+		{
+		case 1:
+			showMenu("create");
+			break;
 
-	// 	case 2:
-	// 		showMenu("read");
-	// 		break;
+		case 2:
+			showMenu("read");
+			break;
 
-	// 	case 3:
-	// 		showMenu("update");
-	// 		break;
+		case 3:
+			showMenu("update");
+			break;
 
-	// 	case 4:
-	// 		showMenu("delete");
-	// 		break;
+		case 4:
+			showMenu("delete");
+			break;
 
-	// 	case 5:
-	// 		break;
+		case 5:
+			break;
 
-	// 	default:
-	// 		cout << "[x] Empty menu.." << endl;
-	// 		break;
-	// 	}
-	// } while (choice != 5);
-
-	listChild LC;
-	addrChild AC;
-
-	AC = alokasi("Daerah A");
-	insertFirst(LC, AC);
-
-	// INF LOOP printInfo
-	printInfo(LC);
+		default:
+			cout << "[x] Empty menu.." << endl;
+			break;
+		}
+	} while (choice != 5);
 	return 0;
 }
